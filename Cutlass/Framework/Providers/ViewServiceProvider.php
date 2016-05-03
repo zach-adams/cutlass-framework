0a60671c5feb78f7500c4adbfc5df4b95e25fdfd
Zach <zach@zach-adams.com>
2016-05-03 11:49:38 -0600

    view service provider

--- Cutlass/Framework/Providers/ViewServiceProvider.php
+++ Cutlass/Framework/Providers/ViewServiceProvider.php
@@ -1,7 +1,61 @@
 <?php namespace Cutlass\Framework\Providers;
 
+use Cutlass\Framework\Application;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\FileViewFinder;
 use Illuminate\View\ViewServiceProvider as ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider {
-	//
+
+
+	/**
+	 * Register the Blade engine implementation.
+	 *
+	 * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+	 * @return void
+	 */
+	public function registerBladeEngine($resolver)
+	{
+		$app = $this->app;
+
+		$this->app->bind('blade.options', function ()
+		{
+			return [
+				'view.compiled' => content_directory() . '/blade-cache',
+			];
+		});
+
+		// The Compiler engine requires an instance of the CompilerInterface, which in
+		// this case will be the Blade compiler, so we'll first create the compiler
+		// instance to pass into the engine so it can compile the views properly.
+		$app->singleton('blade.compiler', function ($app) {
+			$cache = $app['blade.options']['view.compiled'];
+
+			return new BladeCompiler($app['files'], $cache);
+		});
+
+		$resolver->register('blade', function () use ($app) {
+			return new CompilerEngine($app['blade.compiler']);
+		});
+	}
+
+	/**
+	 * Register the view finder implementation.
+	 *
+	 * @return void
+	 */
+	public function registerViewFinder()
+	{
+		$this->app->bind('view.finder', function ($app) {
+			$paths = [];
+
+			foreach ($this->app->getThemes() as $theme)
+			{
+				$paths[] = $theme->getBasePath() . '/resources/views';
+			}
+
+			return new FileViewFinder($app['files'], $paths);
+		});
+	}
 }
